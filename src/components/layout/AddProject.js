ce267401c0fd85a947943c8ee5b0643a1f8b8cc4
Diganta Das <ddas461598@gmail.com>
2020-08-20 00:59:01 +0530

    Added functionality to display and switch beteen multiple ptojects

--- src/components/layout/AddProject.js
+++ src/components/layout/AddProject.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {firebase, firebaseConfig} from '../../firebase';
+import {generatePushId} from '../../helpers';
+import {useProjectValue, useProjectsValue} from '../../context';
 
-function AddProject() {
+function AddProject({shouldShow = false}) {
+    const [show, setShow] = useState(shouldShow);
+    const [projectName, setProjectName] = useState('');
+
+    const projectId = generatePushId();
+    const {setProjects} = useProjectsValue();
+
+    const addProject = () =>
+    {
+        projectName && firebase.firestore().collection('projects')
+        .add({
+            projectId,
+            name: projectName,
+            userId: '23c3rrf',
+        })
+        .then(()=>{
+            setProjects([]);
+            setProjectName('');
+            setShow(false);
+        });
+    }
+
     return (
-        <div>
-            Add Projects
+        <div className='add-projects' data-testid='add-project'>
+            {show && (
+                <div className="add-project__input">
+                    <input value={projectName} type="text" className='add-project__name' data-testid='project-name' placeholder='Name Your Project' onChange={e => setProjectName(e.target.value)}/>
+                    <button className='add-project__submit' type='button' data-testid='add-project-submit'>Add Project</button>
+                    <span className="add-project__cancel" data-testid='hide-project-overlay'
+                    onClick={() => setShow(false)}
+                    >Cancel</span>
+                </div>
+            )}
         </div>
     )
 }
